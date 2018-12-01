a56a0176bae6e7cc1edce1b15e2ad30d8714ba24
JR Tashjian <jrtashjian@gmail.com>
2018-12-01 15:10:38 -0500

    Bumping version to 1.0.1

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Static headings are boring, let’s get them moving.
  * Author: sorta brilliant
  * Author URI: https://sortabrilliant.com
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
