096090f86940c5264c43a22acc5440f36b4724c8
JR Tashjian <jrtashjian@gmail.com>
2020-01-12 19:31:09 -0500

    Removing legacy GitHub updater code

--- plugin.php
+++ plugin.php
@@ -21,27 +21,3 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Block Initializer.
  */
 require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
-
-/**
- * GitHub Plugin Updater.
- */
-function sbb_ghostwriter_github_plugin_updater_test_init() {
-	include_once 'updater.php';
-
-	if ( is_admin() ) {
-		$config = array(
-			'slug'               => plugin_basename( __FILE__ ),
-			'proper_folder_name' => 'sbb-ghostwriter',
-			'api_url'            => 'https://api.github.com/repos/sortabrilliant/ghostwriter',
-			'raw_url'            => 'https://raw.github.com/sortabrilliant/ghostwriter/master',
-			'github_url'         => 'https://github.com/sortabrilliant/ghostwriter',
-			'zip_url'            => 'https://github.com/sortabrilliant/ghostwriter/archive/master.zip',
-			'requires'           => '4.9.8',
-			'tested'             => '4.9.8',
-			'readme'             => 'README.md',
-		);
-
-		new SBB_Ghostwriter_GitHub_Updater( $config );
-	}
-}
-add_action( 'init', 'sbb_ghostwriter_github_plugin_updater_test_init' );
